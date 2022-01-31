f9d7eaf3b55ec65bda2fa2e638cfac196ad46eba
Anand V P <39690774+vpanand7@users.noreply.github.com>
2022-02-01 02:15:06 +0530

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -2,13 +2,13 @@
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBhp4mqfn04u48lAiijqYyC-vBtMcEI888",
-    authDomain: "fir-crod.firebaseapp.com",
-    projectId: "fir-crod",
-    storageBucket: "fir-crod.appspot.com",
-    messagingSenderId: "229633157519",
-    appId: "1:229633157519:web:4aae4bce127ddf39dd43fa",
-    measurementId: "G-57Y67QV177"
+    apiKey: 
+    authDomain: 
+    projectId: 
+    storageBucket: 
+    messagingSenderId: 
+    appId: 
+    measurementId: 
   };
 
 export default firebase.initializeApp(firebaseConfig)
